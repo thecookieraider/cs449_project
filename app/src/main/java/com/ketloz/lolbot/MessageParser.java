fb85d9f3dbf31e147e64fee691d42cf8127590d4
Zach Zoltek <zoltekz@yahoo.com>
2020-03-09 00:57:35 -0500

    logging for message parser class

--- app/src/main/java/com/ketloz/lolbot/MessageParser.java
+++ app/src/main/java/com/ketloz/lolbot/MessageParser.java
@@ -1,21 +1,39 @@
 package com.ketloz.lolbot;
 
+import android.os.Message;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MessageParser {
+    private final static String LOGGER_NAME = "MessageParser";
+    private final static Logger LOGGER = Logger.getLogger(MessageParser.LOGGER_NAME);
     private void exec(String command, List<String> args) {
 
     }
 
+    private static void logMethodEntry(String methodName) {
+        MessageParser.LOGGER.entering("MessageParser", methodName);
+    }
+
+    private static void logMethodExit(String methodName) {
+        MessageParser.LOGGER.exiting("MessageParser", methodName);
+    }
+
     public void parseMessage(String message) {
+        MessageParser.logMethodEntry("parseMessage");
+
         if (message.startsWith("!")) {
             final String[] cmdAndArgs = message.split(" ");
-            final String command = args[0];
+            final String command = cmdAndArgs[0];
             final List<String> args = Arrays.asList(cmdAndArgs).subList(1, cmdAndArgs.length);
             this.exec(command, args);
         }
+
+        MessageParser.logMethodExit("parseMessage");
     }
 
     public boolean isValidMessage(String message) {
